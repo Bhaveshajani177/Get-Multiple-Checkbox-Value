cd0d33ee1d397f0ccc9cb04a25797b58677b403a
bhaveshajani <bhaveshajani177@gmail.com>
2021-06-16 14:48:06 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MatSnackBar,MatSnackBarHorizontalPosition,MatSnackBarVerticalPosition,} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,110 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demo';
+  
+  checked = false;
+  selectedItemsList: any;
+  checkedIDs: any;
+
+  // for snacbar
+  horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
+
+  checkboxesDataList = [
+    {
+      id: 'C001',
+      label: 'Photography',
+      isChecked: false
+    },
+    {
+      id: 'C002',
+      label: 'Writing',
+      isChecked: false
+    },
+    {
+      id: 'C003',
+      label: 'Painting',
+      isChecked: false
+    },
+    {
+      id: 'C004',
+      label: 'Knitting',
+      isChecked: false
+    },
+    {
+      id: 'C004',
+      label: 'Dancing',
+      isChecked: false
+    },
+    {
+      id: 'C005',
+      label: 'Gardening',
+      isChecked: false
+    },
+    {
+      id: 'C006',
+      label: 'Drawing',
+      isChecked: false
+    },
+    {
+      id: 'C007',
+      label: 'Gyming',
+      isChecked: false
+    },
+    {
+      id: 'C008',
+      label: 'Cooking',
+      isChecked: false
+    },
+    {
+      id: 'C009',
+      label: 'Scrapbooking',
+      isChecked: false
+    },
+    {
+      id: 'C010',
+      label: 'Origami',
+      isChecked: false
+    }
+  ]
+
+  constructor(private _snackBar: MatSnackBar) {}
+
+  ngOnInit(): void {
+    this.fetchSelectedItems()
+    this.fetchCheckedIDs()
+  }
+
+  changeSelection() {
+    this.fetchSelectedItems();
+    this.fetchCheckedIDs();
+  }
+
+  fetchSelectedItems() {
+    this.selectedItemsList = this.checkboxesDataList.filter((value, index) => {
+      return value.isChecked;
+    });
+  }
+
+  fetchCheckedIDs() {
+    this.checkedIDs = []
+    this.checkboxesDataList.forEach((value, index) => {
+      if (value.isChecked) {
+        this.checkedIDs.push(value.id);
+      }
+    });
+  }
+
+  submitHobbies(){
+    console.log(this.selectedItemsList);
+    console.log(this.checkedIDs);
+    // show snackbar
+    this._snackBar.open('Successfully, Show result in console', '', {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      duration: 3 * 1000,
+      panelClass: ['snackbar-color']
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// material
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
@@ -10,7 +20,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
